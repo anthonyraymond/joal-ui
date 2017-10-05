e258f901e6aeef2c4aa9bd81701f172022a75ff1
anthonyraymond <anthonyraymond@users.noreply.github.com>
2017-10-05 22:50:12 +0200

    Fix overral speed sum

--- src/components/ClientInfo/clientInfo.container.js
+++ src/components/ClientInfo/clientInfo.container.js
@@ -8,8 +8,12 @@ const calculateGobalSpeed = (announcers) => {
   if (announcers.length === 0) {
     return 0;
   }
+  // We need this weird division because the currentSpeed property is actually the max speed that the torrent would use if there were no other torrents
+  //  but joal-core divide the speed for all torrent that are currently seeding (with more than 0 b/s)
+  const announcersCount = announcers.filter(announcer => announcer.currentSpeed > 0).length;
+  const divider = (announcersCount > 0 ? announcersCount : 1);
   return announcers.reduce((prevVal, announcer) => (
-    prevVal + announcer.currentSpeed
+    prevVal + (announcer.currentSpeed / divider)
   ), 0);
 };
 
