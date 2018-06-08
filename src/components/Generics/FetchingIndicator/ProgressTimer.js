b35174d196d9e79a9cc5627f1b0b3afce0a15e3c
anthonyraymond <anthonyraymond@users.noreply.github.com>
2018-06-08 23:18:25 +0200

    changed line end encoding in src folder

--- src/components/Generics/FetchingIndicator/ProgressTimer.js
+++ src/components/Generics/FetchingIndicator/ProgressTimer.js
@@ -34,12 +34,13 @@ export class ProgressTimer extends React.Component {
     }
   }
 
+  // eslint-disable-next-line class-methods-use-this
   secondToHMS(sec) {
     const h = Math.floor(sec / 3600);
     const m = Math.floor(sec % 3600 / 60);// eslint-disable-line no-mixed-operators
     const s = Math.floor(sec % 3600 % 60);
-    const res = ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
-    return res ? res : '00:00';
+    const res = (`${(h > 0 ? `${h}:${m < 10 ? '0' : ''}` : '') + m}:${s < 10 ? '0' : ''}${s}`);
+    return res || '00:00';
   }
 
   render() {
