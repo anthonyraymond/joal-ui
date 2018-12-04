bd01e2f2ffe518b9688a37412a20ff49f74a679b
anthonyraymond <anthonyraymond@users.noreply.github.com>
2018-12-05 00:23:35 +0100

    Prepare add torrent button

--- src/components/DashBoard/dashboard.component.js
+++ src/components/DashBoard/dashboard.component.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip';
 import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 import { withStyles } from '@material-ui/core/styles';
 import ClientInfo from '../ClientInfo';
 import AbsoluteOverlayFetchingIndicator from '../Generics/FetchingIndicator/AbsoluteOverlayFetchingIndicator';
@@ -9,9 +11,14 @@ import UiConfigChangerButton from '../UiConfigChanger';
 import Announcers from '../Announcers';
 import QueuedTorrents from '../QueuedTorrents';
 
-const styles = () => ({
+const styles = theme => ({
   relative: {
     position: 'relative'
+  },
+  addButton: {
+    position: 'fixed',
+    bottom: 65,
+    right: theme.spacing.unit
   }
 });
 
@@ -51,6 +58,10 @@ const Dashboard = ({
       </Grid>
     )}
     <ReactTooltip place="top" type="dark" effect="float" />
+
+    <Fab className={classes.addButton} variant="fab" disabled={!isStarted} size="medium" color="secondary" aria-label="Add">
+      <AddIcon />
+    </Fab>
   </Grid>
 );
 
