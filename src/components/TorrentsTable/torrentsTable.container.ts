3cca64115d23aab2fe489e167c84a93689bcb379
anthonyraymond <anthonyraymond@users.noreply.github.com>
2019-05-29 00:47:10 +0200

    Improve table search & filter performance

--- src/components/TorrentsTable/torrentsTable.container.ts
+++ src/components/TorrentsTable/torrentsTable.container.ts
@@ -3,38 +3,45 @@ import { connect } from 'react-redux';
 import Announcers from './torrentsTable.component';
 import { changeSearchFilterText, changeTorrentSort } from './torrentsTable.actions';
 import { deleteTorrent } from '../../modules/joal-api';
+import { createSelector } from 'reselect'
 
 import { JoalState } from '../../reducers/types';
 import { Announcer as AnnouncerType } from '../../modules/joal-api/types';
 
-const filterTorrents = (torrents: Array<AnnouncerType>, searchFilter: string) => {
-  if (searchFilter.trim().length === 0) {
-    return torrents;
+const getSearchFilter = (state: JoalState) => state.app.torrentsTable.searchFilter;
+const getAnnouncers = (state: JoalState) => state.api.announcers;
+const getFilteredAnnouncers = createSelector(
+  [ getSearchFilter, getAnnouncers ],
+  (searchFilter: string, announcers: Array<AnnouncerType>) => {
+    if (searchFilter.trim().length === 0) {
+      return announcers;
+    }
+    const filterText = searchFilter.toLowerCase();
+    return announcers.filter(a => a.torrentName.toLowerCase().includes(filterText));
   }
-  const filterText = searchFilter.toLowerCase();
-  return torrents.filter(t => t.torrentName.toLowerCase().includes(filterText));
-};
+)
 
-const sortTorrents = (torrents: Array<AnnouncerType>, sortProperty: string, sortDirection: string) => {
-  if (sortProperty === '' || sortProperty === undefined || sortProperty === null) {
-    return torrents;
+const getSortProperty = (state: JoalState) => state.app.torrentsTable.sortProperty;
+const getSortDirection = (state: JoalState) => state.app.torrentsTable.sortDirection;
+const getSortedAndFilteredAnnouncers = createSelector(
+  [ getSortProperty, getSortDirection, getFilteredAnnouncers ],
+  (sortProperty: string, sortDirection: string, filteredAnnouncers: Array<AnnouncerType>) => {
+    if (sortProperty === '' || sortProperty === undefined || sortProperty === null) {
+      return filteredAnnouncers;
+    }
+
+    // Spread the array to sort on a copy of the original, it will prevent side effects on the original array
+    return [...filteredAnnouncers].sort((t1: any, t2: any) => {
+      if (t1[sortProperty] < t2[sortProperty]) return sortDirection === 'asc' ? -1 : 1;
+      if (t1[sortProperty] === t2[sortProperty]) return 0;
+      return sortDirection === 'asc' ? 1 : -1;
+    });
   }
-
-  // Spread the array to sort on a copy of the original, it will prevent side effects on the original array
-  return [...torrents].sort((t1: any, t2: any) => {
-    if (t1[sortProperty] < t2[sortProperty]) return sortDirection === 'asc' ? -1 : 1;
-    if (t1[sortProperty] === t2[sortProperty]) return 0;
-    return sortDirection === 'asc' ? 1 : -1;
-  });
-};
+)
 
 function mapStateToProps(state: JoalState) {
   return {
-    announcers: sortTorrents(
-      filterTorrents(state.api.announcers, state.app.torrentsTable.searchFilter),
-      state.app.torrentsTable.sortProperty,
-      state.app.torrentsTable.sortDirection
-    ),
+    announcers: getSortedAndFilteredAnnouncers(state),
     searchFilter: state.app.torrentsTable.searchFilter,
     sortProperty: state.app.torrentsTable.sortProperty,
     sortDirection: state.app.torrentsTable.sortDirection,
